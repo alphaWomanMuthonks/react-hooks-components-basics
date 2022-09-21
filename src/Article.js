534638390a8e4d7eeb9db336672cca4747d26734
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-09-21 11:48:35 +0300

    Learning components how to write ,use and name them

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Article() {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>   // React creates this element and adds it to the DOM,
   );
 }
 
